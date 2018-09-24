c24fd9dded94aa2c471d2d0d3be0c9a240c69363
John Lange <lange.john.m@gmail.com>
2018-09-24 15:34:17 -0400

    750 Words file initiation w/gitignore file

---
+++ 750Words/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_750Words.Startup))]
+namespace _750Words
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
