79b4ed15e61f448da36875ec9bdcf2f7380596d9
John Lange <lange.john.m@gmail.com>
2018-09-24 16:57:27 -0400

    Added word count column, rebuilt db

--- 750Words/Models/Word.cs
+++ 750Words/Models/Word.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace _750Words.Models
 {
-    public class Counter
+    public class Word
     {
+
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int WordCount { get; set; }
+
+    }
+
+    public class SevenFifty : DbContext
+    {
+        public DbSet<Word> Words { get; set; }
+
     }
 }
